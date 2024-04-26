cc590833f99c37f67e82a1be6e3329d000ea6c02
tomdotgillis <Tomdotgillis@gmail.com>
2024-04-26 13:03:09 -0400

    Update JSON2graphML.py

--- JSON2graphML.py
+++ JSON2graphML.py
@@ -1,8 +1,5 @@
-# source .env/bin/activate
-
 import networkx as nx
 import json
-
 
 # Load the JSON data from file
 with open('/Users/tomgillis/Desktop/SMC/Y4/S2/Classes/MA-217/Final Project/starwars-full-interactions-allCharacters-merged.json', 'r') as f:
@@ -10,27 +7,20 @@ with open('/Users/tomgillis/Desktop/SMC/Y4/S2/Classes/MA-217/Final Project/starw
     
 # Create a new graph
 G = nx.Graph()
-'''
-node_name = str(input("Key for Nodes: "))
-node_sub = str(input("Subkey for Nodes: "))
-edge_name = str(input("Key for Edges: "))
-'''
-node_name = 'nodes'
-node_sub = 'name'
-edge_name = 'links'
-# Add nodes
-for node_data in data[node_name]:
-    G.add_node(node_data[node_sub], **node_data)
+
+# Add nodes with numerical IDs
+for idx, node_data in enumerate(data['nodes']):
+    G.add_node(idx, **node_data)
 
 # Add edges
-for edge_data in data[edge_name]:
+for edge_data in data['links']:
     G.add_edge(edge_data['source'], edge_data['target'], **edge_data)
 
 # Write the graph to a GraphML file
-g_name = str(input('\nName the file: '))+'.graphml'
+g_name = input('\nName the file: ') + '.graphml'
 try:
-    print("nx.write_graphml(G, g_name)")
-except:
-    print("Something Went Wrong")
+    nx.write_graphml(G, g_name)
+except Exception as e:
+    print("Something Went Wrong:", e)
 else:
-    print(f"\n\n%s, saved in project dir.\n\n" % g_name)
+    print(f"\n\n{g_name}, saved in project dir.\n\n")
